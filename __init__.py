eec568d8a6e2a4d20c7107e32c2684933c27af3b
tommyso <31903800+tommyso@users.noreply.github.com>
2017-12-13 23:44:41 -0500

    "init" de la aplicación final

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/<name>")
+def index(name=None):
+    return render_template('Review.html', name=name)
+
+@app.route("/end")
+def end():
+    return render_template('return.html')
+
+if __name__ == "__main__":
+app.run()
